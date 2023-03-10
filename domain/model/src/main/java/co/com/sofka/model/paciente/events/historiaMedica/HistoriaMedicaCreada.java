d4e9eae531b605e4632c53657c5add75ea6d5e35
LilianaGallego <lilianagallegom@gmail.com>
2023-03-10 16:12:10 -0500

    new: modificaciones de los modelos

--- domain/model/src/main/java/co/com/sofka/model/paciente/events/historiaMedica/HistoriaMedicaCreada.java
+++
@@ -1,4 +0,0 @@
-package co.com.sofka.model.paciente.events.historiaMedica;
-
-public class HistoriaMedicaCreada {
-}
--- domain/model/src/main/java/co/com/sofka/model/paciente/values/HistoriaMedicaId.java
+++
@@ -1,6 +0,0 @@
-package co.com.sofka.model.paciente.values;
-
-import co.com.sofka.model.paciente.generic.Identity;
-
-public class HistoriaMedicaId extends Identity {
-}
